ac58ab0431b85d01b3ed4bca09d6800d8e795c9d
Rey Garcia <51029045+reygarcia-develops@users.noreply.github.com>
2024-07-30 10:30:40 -0500

    Update about-me.component.ts
    about me updates

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -11,8 +11,8 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 export class AboutMeComponent {
   public readonly summary: string[] = [
     "Hello! I'm Rey Garcia, a full-stack developer with over 10 years of experience in the industry. Throughout my career, I’ve had the opportunity to work with a wide range of technologies. In addition to being a software engineer, I also balance life as a husband and father.",
-    "I began my journey at H-E-B right after college, and since then, I’ve accumulated valuable experience. My passion lies in creating innovative and user-friendly solutions that make a positive impact. Whether it's developing responsive web applications, optimizing backend processes, or ensuring seamless user experiences, I strive to deliver excellence in every project I undertake.",
-    "Fast forward to today, I’ve had the privilege of being part of a team that helped sideline one of our mainframe systems, modernizing our infrastructure and enhancing efficiency. This experience has deepened my understanding of system architecture and the importance of scalable solutions.",
+    "I began my journey at H-E-B right after college, and since then, I have accumulated valuable experience. My passion lies in creating innovative and user-friendly solutions that make a positive impact. Whether it is developing responsive web applications, optimizing backend processes, or ensuring seamless user experiences, I strive to deliver excellence in every project I undertake.",
+    "Fast forward to today, I have had the privilege of being part of a team that first introduced Angular 1 to H-E-B to build an e-commerce site for flowers. I have also worked on optimizing coupon management, creating a new API for managing supplier data in Haskell, and developing a full-stack application to transition business users from the mainframe to a modern solution. This experience has deepened my understanding of system architecture and the importance of scalable solutions.",
     "I hope this has piqued your interest. Let’s connect and explore how we can create impactful solutions together!"
   ];
 }
