017ae97817e37befed676d337732f37134770916
Rey Garcia <garcia.rey@heb.com>
2024-07-31 22:43:46 -0500

    .main and section padding fixes. Animation timing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,7 +32,7 @@ export class AppComponent implements AfterViewInit {
     const options = {
       root: null, //relative to document viewport
       rootMargin: '0px',
-      threshold: 0.1 // 25% of the element in view
+      threshold: 0.25 // 25% of the element in view
     }
 
     const observer = new IntersectionObserver(this.handleIntersect.bind(this), options);
@@ -49,8 +49,14 @@ export class AppComponent implements AfterViewInit {
     // Track the number of sections in view
     const visibleSections = entries.filter(entry => entry.isIntersecting);
     const multipleInView = visibleSections.length >= 2;
-  
-    entries.forEach(entry => {
+
+    const sortedEntries = entries.sort((a, b) => {
+      const indexA = this.sections?.toArray().findIndex(section => section.nativeElement === a.target) ?? 0;
+      const indexB = this.sections?.toArray().findIndex(section => section.nativeElement === b.target) ?? 0;
+      return indexA - indexB;
+    });
+
+    sortedEntries.forEach(entry => {
       const element = entry.target as HTMLElement;
       const title = element.querySelector('.title') as HTMLElement;
       const divider = element.querySelector('.divider') as HTMLElement;
@@ -58,15 +64,13 @@ export class AppComponent implements AfterViewInit {
   
       if (entry.isIntersecting) {
         const index = this.sections?.toArray().findIndex(section => section.nativeElement === entry.target);
-        const baseDelay = 2; // Base delay value
-        const incrementalDelay = 1.15; // Incremental delay value
-        const titleDelay = multipleInView ? baseDelay + (index * incrementalDelay) : 1 ; 
-        const dividerDelay = titleDelay + .8;
+        const titleDelay = index === 0 ? 1.8 : .5; 
+        const dividerDelay = titleDelay + .93;
         const componentDelay = dividerDelay + .8; 
   
         if (title) {
-          title.style.setProperty('--transition-delay', `${titleDelay}s`);
-          title.classList.add('visible');
+          title.style.setProperty('--title-delay', `${titleDelay}s`);
+          title.classList.add('animate');
         }
   
         if (divider) {
